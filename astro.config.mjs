ef4332ce682435e4af01ff04aabeae9c298f6199
Joshua Johnson <johnson.c.joshua@gmail.com>
2026-06-20 20:33:29 -0400

    fixed vercel build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
-import pagefind from 'astro-pagefind';
 
 export default defineConfig({
   site: 'https://brrs-kitchen.com',
   output: 'server',
   adapter: vercel(),
-  integrations: [pagefind()],
 });
