49884a8ab07e8590a7e4506034edbeba8a44c863
Joshua Johnson <johnson.c.joshua@gmail.com>
2026-06-21 21:45:11 -0400

    new recipe

---
+++ content/recipes/desserts/from-scratch-pumpkin-chocolate-chip-cookies.m
@@ -0,0 +1,64 @@
+---
+title: From Scratch Pumpkin Chocolate Chip Cookies
+category: Desserts
+pubDate: '2026-06-21'
+prepTime: 25 min
+cookTime: 11-13 min
+intro: These pumpkin chocolate chip cookies are absolutely delicious. You'll find that using all the individual ingredients vs just pumpkin pie spice makes a world of difference and creates a perfectly spiced classic pumpkin flavor. 
+image: /images/recipes/pumpkin-chocolate-chip-cookies.jpg
+yield: 40 cookies
+ingredients:
+  - count: 3 Cups (325g)
+    item: All-Purpose Flour
+  - count: 3 tsp
+    item: Baking Powder
+  - count: 1-1/8 tsp
+    item: Baking Soda
+  - count: 2-1/4 tsp
+    item: Ground Cinnamon
+  - count: 3/4 tsp
+    item: Ground Ginger
+  - count: 3/4 tsp
+    item: Ground Nutmeg
+  - count: 1/8 tsp
+    item: Ground Cloves (or 1/4 tsp for extra spice)
+  - count: 3/4 tsp
+    item: Salt
+  - count: 3/4 Cup (100g)
+    item: Granulated Sugar
+  - count: 3/4 Cup (110g)
+    item: Packed Light-Brown Sugar or Dark Brown Sugar (170g)
+  - count: 3/4 Cup (120ml)
+    item: Melted Butter
+  - count: 1 Large
+    item: Egg
+  - count: 1
+    item: Egg Yolk
+  - count: 1-1/2 tsp
+    item: Vanilla Extract
+  - count: 1 Can (360-380g)
+    item: Pumpkin Puree
+  - count: 2 Cups
+    item: Semi-Sweet Chocolate Chips
+directions:
+  - title: Preheat and Prepare
+    body: Preheat oven to 350 degrees. Line baking sheets with silicone baking mats or parchment paper.
+  - title: Whisk Dry Ingredients
+    body: In a mixing bowl, whisk together flour, baking powder, baking soda, cinnamon, nutmeg, ginger, cloves and salt for 20 seconds, set aside.
+  - title: Blend Sugars and Butter
+    body: In the bowl of an electric stand mixer fitted with the paddle attachment, blend together granulated sugar, brown sugar and melted butter until combined.
+  - title: Add Wet Ingredients
+    body: Mix in egg and vanilla then blend in pumpkin puree.
+  - title: Combine Flour and Wet Mixtures
+    body: With mixer on low speed, slowly add in flour mixture and mix just until nearly combined, scraping down the sides of the bowl as needed (batter will be pretty wet).
+  - title: Fold in Chocolate Chips
+    body: With a rubber spatula, fold in chocolate chips while folding batter to evenly combined.
+  - title: Scoop Dough
+    body: Drop dough 2 Tbsp (either using a medium cookie dough scoop or using two spoons) at a time onto baking sheets lined with Silcone liners or parchment paper, spacing cookies two inches apart.
+  - title: Bake
+    body: Bake in preheated oven in the middle racks until cookies are nearly set about 11 - 13 minutes.
+  - title: Cool and Store
+    body: |-
+      Allow to cool several minutes before transferring to a wire rack to cool.
+      Store in a single layer in an airtight container (these are best once they've rested in an airtight container a few hours, they soften throughout).
+---
